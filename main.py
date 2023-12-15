d91b8a7fc3739aa0337340a419dea811eaa7786f
SkomisLTU <s.str18@gmail.com>
2023-12-15 19:25:30 +0200

    Pridetas spalvotas tekstas, meniu pasirinkimai, zaidimas su botu.

--- main.py
+++ main.py
@@ -3,56 +3,124 @@
 ## kryziukai nuliukai zaidimas
 #
 '''
-saltiniai:
-
-cursor position:
-https://stackoverflow.com/questions/3698635/how-to-get-the-text-cursor-position-in-windows
-
-kaip isvalyti konsole:
+spalvotas terminal tekstas:
+https://stackoverflow.com/questions/287871/how-do-i-print-colored-text-to-the-terminal
 '''
 
-from modules.matrica import matrica
-import os
+### importai
+from modules.matrica import Matrica
+import os as win10
+import time
 
-rezultatas = {"Kryžiukai": 0, "Nuliukai": 0}
 ejimas = 0
 laimejo = False
 
-### exe failui
+
+### galima pridet exe failui, pycharmo konselej neveikia
 def cls():
-    os.system('cls' if os.name=='nt' else 'clear')
-
-zaidimas1 = matrica()
-zaidimas1.pridetRema(ejimas)
-
-# kol nelaimejo
-while not laimejo:
-    # X ejimas
-
-    zaidimas1.uzbraukti("X")
-    ejimas = zaidimas1.pridetRema(ejimas)
-    if ejimas > 4:   # nes laimeti galima tik po 5 ejimo
-        if zaidimas1.arlaimejo("X"):
-            print("Kryziukai laimojo! :) ")
-            break
-        elif ejimas > 8:
-            print("Lygiosios! :) ")
-            break
-
-    # Y ejimas
-
-    zaidimas1.uzbraukti("O")
-    ejimas = zaidimas1.pridetRema(ejimas)
-    if ejimas > 4:
-        if zaidimas1.arlaimejo("O"):
-            print("Nuliukai laimojo! :) ")
-            break
+    win10.system('cls' if win10.name=='nt' else 'clear')
 
 
+def meniu():
+    print("iveskite komandą:")
+    print("z -> žaisti 2 žaidėjams\tk -> žaisti su kompiuteriu")
+    print("i -> info kaip žaisti\tu -> uždaryti programą")
+    komanda = input("")
+    match komanda:
+        case "z":
+            ejimas = 0
+            zaidimas1 = Matrica()
+            zaidimas1.pridetRema(ejimas)
+            zaidimas1.vardai[0] = input("1 žaidėjo vardas:\t")
+            zaidimas1.vardai[1] = input("2 žaidėjo vardas:\t")
+            zaidimas1.vardai[1][0] =zaidimas1.vardai[1][1].upper()
+            zaidimas1.vardai[1][1] =zaidimas1.vardai[1][1].upper()
+            zaidziam(zaidimas1, ejimas, result=[0,0]) ## vardu nemetam, nes imetem visa objekta zaidimas1
+
+        case "u":
+            print("Ar tikrai norite uždaryti programą? :(\n y - taip\t n - ne")
+            komanda2 = input("")
+            if komanda2 == "y":
+                print("Dėkojame, jog žaidėte :)")
+                time.sleep(3)
+                exit()
+            else:
+                meniu()
+        case "k":
+            ejimas = 0
+            zaidimas1 = Matrica()
+            zaidimas1.pridetRema(ejimas)
+            zaidimas1.vardai[0] = input("1 žaidėjo vardas:\t")
+            zaidimas1.vardai[1] = "Kompiuteris " + win10.getcwd().split("\\")[2]  ## kompo pavadinimas
+            zaidziam(zaidimas1, ejimas, False, "X", "K")  ## vardu nemetam, nes imetem visa objekta zaidimas1
+        case "i":
+            if win10.name == "nt":   ### win10 pavadinimas
+                try:
+                    win10.system("start \"\" https://www.youtube.com/watch?v=0y0-xr2Af4w")
+                except:
+                    print("nepavyko atidaryti :(")
+            elif win10.name == "pofix":       ### linux ir macos
+                try: ## linux
+                    win10.system("xdg-open \"\" https://www.youtube.com/watch?v=0y0-xr2Af4w")
+                except:
+                    print("nepavyko atidaryti :(")
+                try:  ## macOs
+                    win10.system("open \"\" https://www.youtube.com/watch?v=0y0-xr2Af4w")
+                except:
+                    print("nepavyko atidaryti :(")
+        case _:
+            print("Bloga komanda. Rinkitės dar kartą")
+            meniu()
+
+def zaidziam(zaidimas: Matrica, ejimas, pradedaO = False, zaid1= "X", zaid2= "O", result= [0,0]):
+    print("res ", result, "  ", end="")
+    b = pradedaO
+    while not laimejo:
+        # X ejimas
+        if not b:   ### jeigu pradeda ne nuliukai
+            zaidimas.uzbraukti(zaid1)
+            ejimas = zaidimas.pridetRema(ejimas)
+            if ejimas > 4:  # nes laimeti galima tik po 5 ejimo
+                if zaidimas.arlaimejo(zaid1):
+                    ## teksto spalva, vardas, tekstas, spalvos pabaiga
+                    print(Matrica.VIOLETINE, zaidimas.vardai[0], " laimėjo! :) ", Matrica.ENC)
+                    result[0] += 1
+                    break
+                elif ejimas > 8:
+                    print(Matrica.ZALIA, "Lygiosios! :) ", Matrica.ENC)
+                    break
+        b = False
+        # O ejimas
+
+        zaidimas.uzbraukti(zaid2)
+        ejimas = zaidimas.pridetRema(ejimas)
+        if ejimas > 4:
+            if zaidimas.arlaimejo(zaid2):
+                print(Matrica.GELTONA, zaidimas.vardai[1], " laimėjo! :) ", Matrica.ENC)
+                result[1] += 1
+                break
 
 
+    ### kita zaidima zaidejai apsikeicia
+    print("ar žaisti dar kartą?\n [y > taip,n > ne]")
+    match input():
+        case "y":
+            ## X su O apsikeicia vietomis, klase persiraso ir vel zaidzia
+            print("Rezultatai: ", zaidimas.vardai[0], ": ", result[0], "\n         ",
+                  zaidimas.vardai[1], ": ", result[1])
+            zaidimas.__init__()
+            zaidimas.pridetRema(0)
+            zaidziam(zaidimas, 0, not(pradedaO), zaid1, zaid2, result=[result[0],result[1]])
+        case _:
+            print("Rezultatai: ", zaidimas.vardai[0], ": ", result[0], "\n         ",
+                  zaidimas.vardai[1], ": ", result[1])
+            meniu() ## inicijuojam is naujo
 
 
+##
+### init
+##
 
-
+print("\t\tSveiki atvykę!")
+meniu()
 
--- modules/matrica.py
+++ modules/matrica.py
@@ -1,35 +1,66 @@
-class matrica:
+import msvcrt
+import time
+
+
+class Matrica:
+
+    RAUDONA   = '\033[91m'
+    ZALIA   = '\033[92m'
+    GELTONA     = '\033[93m'
+    MELYNA    = '\033[94m'
+    VIOLETINE = '\033[95m'
+    ENC = '\033[0m'   #  end of colour
+    vardai = ["",""]  ## indeksas 0 -> iksiuku zaidejas, 1 -> nuliuku zaidejas
+
+
 
     def __init__(self):
-        print("\n"*10)
+        print("\n"*10)   ### kad nesimaisytu senos lenteles
         self.pradinis = ["1", "2", "3",
                          "4", "5", "6",
                          "7", "8", "9"]
 
     def uzbraukti(self, ka_braukti):
 
-
         self.ka_braukti = ka_braukti
+
         a = ""
+
         while 1:
             if self.ka_braukti == "X":
-                a = input("Žaidžia Kryziukai:\t")
+                ## zodis, spalva, vardas, end of colour
+                print("Žaidžia ", self.VIOLETINE, self.vardai[0], ":\t", self.ENC)
+                a = input()
+                spal = self.VIOLETINE
             elif self.ka_braukti == "O":
-                a = input("Žaidžia nuliukai:\t")
+                print("Žaidžia ", self.GELTONA, self.vardai[1], ":\t", self.ENC)
+                a = input()
+                spal = self.GELTONA
+            elif self.ka_braukti == "K": ## kompiuteris
+                print(self.vardai[1], " galvoja")
+                time.sleep(2)
+                count = (int(time.time_ns()/1000)%9)  ## random skaicius naudojant sistemos laika
+                ## ziuri ar sugeneruotas ejimas laisvas
+                while not self.pradinis[count] == str(count+1):
+                    count = int(time.time_ns() / 1000) % 9
+                a = str(count+1)
+                spal = self.MELYNA
             else:
                 print("kazkas blogai")
                 break
         ### paziuri ar ivestas skaicius nuo 1 iki 9 ir ar nesikartoja
             if a.isdigit() and int(a) >=1 and int(a) < 10 and self.pradinis[int(a)-1].isdigit():
-                self.pradinis[int(a)-1] = ka_braukti
+                if (ka_braukti == "K"):  ### jeigu zaidziam su kompu vistiek rasom nuliuka
+                    ka_braukti = "O"
+                self.pradinis[int(a)-1] = spal + ka_braukti + self.ENC    ## spalva + keiciamas skaicius + end of colour
                 break
             else:
-                print("blogas ejimas")
+                print("blogas ejimas, spauskite dar kartelį")
 
 
     def pridetRema(self, kartas):
-
-        s = "_______________\n"
+        s = "\n" * 5
+        s += "_______________\n"
         s += self.pradinis[6]   # 7
         s += "  |  "
         s += self.pradinis[7]   # 8
@@ -56,14 +87,14 @@ class matrica:
 
     def arlaimejo(self, kas):
         laimejo = False
-        if ((self.pradinis[0] == kas and self.pradinis[1] == kas and self.pradinis[2] == kas)    # tikrinam eilutes
-            or (self.pradinis[3] == kas and self.pradinis[4] == kas and self.pradinis[5] == kas) #
-            or (self.pradinis[6] == kas and self.pradinis[7] == kas and self.pradinis[8] == kas) ####
-            or (self.pradinis[0] == kas and self.pradinis[3] == kas and self.pradinis[6] == kas) # tikrinam stulpelius
-            or (self.pradinis[1] == kas and self.pradinis[4] == kas and self.pradinis[7] == kas) #
-            or (self.pradinis[2] == kas and self.pradinis[5] == kas and self.pradinis[8] == kas) #
-            or (self.pradinis[0] == kas and self.pradinis[4] == kas and self.pradinis[8] == kas) # tikrinam istrizaines
-            or (self.pradinis[6] == kas and self.pradinis[4] == kas and self.pradinis[2] == kas)): #
+        if ((self.pradinis[0].count(kas) and self.pradinis[1].count(kas) and self.pradinis[2].count(kas))    # tikrinam eilutes
+            or (self.pradinis[3].count(kas) and self.pradinis[4].count(kas) and self.pradinis[5].count(kas)) #
+            or (self.pradinis[6].count(kas) and self.pradinis[7].count(kas) and self.pradinis[8].count(kas)) ####
+            or (self.pradinis[0].count(kas) and self.pradinis[3].count(kas) and self.pradinis[6].count(kas)) # tikrinam stulpelius
+            or (self.pradinis[1].count(kas) and self.pradinis[4].count(kas) and self.pradinis[7].count(kas)) #
+            or (self.pradinis[2].count(kas) and self.pradinis[5].count(kas) and self.pradinis[8].count(kas)) #
+            or (self.pradinis[0].count(kas) and self.pradinis[4].count(kas) and self.pradinis[8].count(kas)) # tikrinam istrizaines
+            or (self.pradinis[6].count(kas) and self.pradinis[4].count(kas) and self.pradinis[2].count(kas))): #
             laimejo = True
 
         return laimejo
